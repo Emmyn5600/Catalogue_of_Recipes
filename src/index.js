caa81bf378ca9d639e5dbde107ba466518f55c52
emmyn5600 <emmy6618@gmail.com>
2021-08-05 15:45:28 +0200

    Add index.js in the reducers folder to cmoboie  redures

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Route from './components/Route';
 
+const store = createStore(reducer)
+
 ReactDOM.render(
   <React.StrictMode>
     <Route />
---
+++ src/reducers/recipes.jsx
@@ -0,0 +1,10 @@
+const recipes = (state = [], action) => {
+  switch (action.type) {
+    case 'LOAD_RECIPES':
+        return state;
+    default:
+      return state;
+  }
+};
+
+export default recipes;
