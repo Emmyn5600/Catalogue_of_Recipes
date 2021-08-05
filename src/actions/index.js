280dc18190c0965fd34c12fe8cf1053ebc4cc2b8
emmyn5600 <emmy6618@gmail.com>
2021-08-05 19:58:02 +0200

    Add the categoriesEndpoint

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,29 @@
+import axios from 'axios';
+
 const endPoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const categoriesEndPoint = 'https://www.themealdb.com/api/json/v1/1/categories.php';
 
 export const LOAD_RECIPES_SUCCESS = 'LOAD_RECIPES_SUCCESS';
 export const LOAD_RECIPES_ERROR = 'LOAD_RECIPES_ERROR';
+export const LOAD_CATEGORIES_SUCCESS = 'LOAD_CATEGORIES_SUCCESS';
+export const LOAD_CATEGORIES_FAILURE = 'LOAD_CATEGORIES_FAILURE';
+export const LOAD_CATEGORIES_START = 'LOAD_CATEGORIES_START';
+
+const loadCategoriesSuccess = (categories) => ({
+  type: LOAD_CATEGORIES_SUCCESS,
+  payload: categories,
+});
+
+const loadCategoriesFailure = (error) => ({
+  type: LOAD_CATEGORIES_FAILURE,
+  payload: error,
+});
+
+const loadCategoriesStart = () => ({
+  type: LOAD_CATEGORIES_START,
+});
 
 export const loadRecipes = () => async (dispatch) => {
-  console.log('loadRecipes');
   try {
     const res = await fetch(endPoint);
     const data = await res.json();
@@ -12,4 +31,15 @@ export const loadRecipes = () => async (dispatch) => {
   } catch (error) {
     dispatch({ type: LOAD_RECIPES_ERROR, payload: error.message });
   }
+};
+
+export const loadCategoriesAsync = () => async (dispatch) => {
+  dispatch(loadCategoriesStart());
+  try {
+    const response = await axios.get(categoriesEndPoint);
+    dispatch(loadCategoriesSuccess(response.data.categories));
+  } catch (error) {
+    dispatch(loadCategoriesFailure(error.message));
+  }
+  axios.get(endPoint);
 };
