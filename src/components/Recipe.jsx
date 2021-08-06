1312cfa851c2e2d32574761d16bdcd8836156d62
emmyn5600 <emmy6618@gmail.com>
2021-08-06 19:57:42 +0200

    Add some styling

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -9,23 +9,25 @@ const Recipe = (props) => {
   } = recipe;
 
   return (
-    <Link to={`recipes/${idMeal}`} className="recipe-item-container">
-      <div className="image-wrapper">
-        <img src={strMealThumb} alt="recipe" />
-      </div>
-      <div className="recipe-details">
-        <h3 className="name">{strMeal}</h3>
-        <div className="category-area-container d-flex flex-center flex-between">
-          <div className="category">
-            <span>{strCategory}</span>
-          </div>
-          <h4 className="from">
-            From:
-            <span>{strArea}</span>
-          </h4>
+    <div className="card">
+      <Link to={`recipes/${idMeal}`} className="recipe-item-container">
+        <div className="image-wrapper">
+          <img src={strMealThumb} alt="recipe" />
         </div>
-      </div>
-    </Link>
+        <div className="recipe-details">
+          <h3 className="name">{strMeal}</h3>
+          <div className="category-area-container d-flex flex-center flex-between">
+            <div className="category">
+              <span>{strCategory}</span>
+            </div>
+            <h4 className="from">
+              From:
+              <span>{strArea}</span>
+            </h4>
+          </div>
+        </div>
+      </Link>
+    </div>
   );
 };
 
